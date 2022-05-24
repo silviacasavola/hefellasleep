b1e55cfb1114ef8d83d02a632c2c39dfb608b470
silviacasavola <silcasavola@gmail.com>
2022-05-24 22:07:18 +0200

    updated

--- assets/js/autoscroll.js
+++ assets/js/autoscroll.js
@@ -1,3 +1,4 @@
+let a=2;
 history.scrollRestoration = "manual";
 $(window).on('beforeunload', function(){
   $("body").scrollTop(0);
@@ -12,9 +13,25 @@ function startscrolling () {
 setInterval(
 function addScroll () {
 let  scroll1 = $(window).scrollTop();
-  scroll = scroll1 + 2;
+  scroll = scroll1 + a;
 
 window.scrollTo({top: scroll, behavior: 'smooth'});
-}, 50)
+}, 40)
 
 }
+
+document.addEventListener('mousedown', e => {
+a=0
+});
+document.addEventListener('mouseup', e => {
+  setTimeout(keepScrolling,1000)
+
+  });
+
+  document.addEventListener('wheel', e => {
+    a=0
+    setTimeout(keepScrolling,1000)
+    });
+ function keepScrolling(){
+   a=1
+ }
